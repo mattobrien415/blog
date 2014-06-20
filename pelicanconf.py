1637944f1b8dc3b93af17309cedcaa42fed4ce88
Matt O'Brien <enuffodis@gmail.com>
2014-06-20 11:50:54 -0700

    edited

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,6 +18,10 @@ TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = u'en'
 
+
+GOOGLE_ANALYTICS = 'UA-51519407-1'
+
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
