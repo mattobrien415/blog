9990e830e9c827db8773833f9a575ae29adea0ee
Matt O'Brien <enuffodis@gmail.com>
2014-07-12 21:00:21 -0700

    edited

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 PLUGIN_PATH = "pelican-plugins"
 
 #THEME = "themes/notmyidea-cms"
-THEME = "themes/tuxlite_tbs"
+THEME = "themes/brownstone"
 
 AUTHOR = u"Matt O'Brien"
 SITENAME = u"Matt O'Brien (dot) Me"
